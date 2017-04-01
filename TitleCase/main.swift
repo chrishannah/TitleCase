275e59b9bb69a1984fe67de0b4fc7a2e006a8781
Christopher Hannah <christopher.hannah@me.com>
2017-04-01 20:23:03 +0100

    initial build

--- TitleCase/main.swift
+++ TitleCase/main.swift
@@ -8,5 +8,49 @@
 
 import Foundation
 
-print("Hello, World!")
+func printUsage() {
+    print("Usage: title <title>")
+    print("")
+    print("Example:")
+    print("")
+    print("    title \"This is the title i want to convert\"")
+    print("")
+}
 
+func getTitleCase(_ title: String) -> String {
+    
+    let disallowedCharacters = CharacterSet(charactersIn: "!*'();:@&=$,/\\?%#[]`~\"")
+    let spaceSafe = title.replacingOccurrences(of: " ", with: "+")
+    let safe = spaceSafe.addingPercentEncoding(withAllowedCharacters: disallowedCharacters.inverted)!
+    
+    let urlPath = "http://brettterpstra.com/titlecase/?title=\(safe)"
+    let url: URL = URL(string: urlPath)!
+    let session = URLSession.shared
+    
+    var title: String = ""
+    let semaphore = DispatchSemaphore(value: 0);
+    
+    let task = session.dataTask(with: url, completionHandler: {data, response, error -> Void in
+        
+        if data != nil {
+            let string = String(data: data!, encoding: String.Encoding.ascii)
+            title = string!
+        }
+        semaphore.signal()
+        
+    })
+    task.resume()
+    
+    _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+    
+    return title
+}
+
+let arguments = CommandLine.arguments
+
+if arguments.count == 2 {
+    let title = arguments[1]
+    print(getTitleCase(title))
+} else {
+    printUsage()
+}
