00d2e7fd8f0c39585d371d068938d15dde7dea2e
Christopher Hannah <christopher.hannah@me.com>
2017-04-01 20:11:52 +0100

    Initial Commit

---
+++ TitleCase/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TitleCase
+//
+//  Created by Christopher Hannah on 01/04/2017.
+//  Copyright © 2017 Christopher Hannah. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
